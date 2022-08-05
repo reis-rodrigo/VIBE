3e289243af9b6721a72f27f3ea74a7698f00c4fe
Rodrigo Gebara Reis <reis.reisrodrigo@usp.br>
2022-08-05 19:15:39 -0400

    add

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+mkdir -p yolo/config
+mkdir -p yolo/data
 mkdir -p data
 cd data
-gdown "https://drive.google.com/uc?id=1untXhYOLQtpNEy4GTY_0fL_H-k6cTf_r"
+gdown "https://drive.google.com/uc?export=download&id=1JSLHXlOcgNCVu6mo2JFzzyj--7dioZnn"
 unzip vibe_data.zip
 rm vibe_data.zip
 cd ..
 mv data/vibe_data/sample_video.mp4 .
-mkdir -p $HOME/.torch/models/
-mv data/vibe_data/yolov3.weights $HOME/.torch/models/
+mv data/vibe_data/yolov3.weights ./yolo/config/
+mv data/vibe_data/yolov3.cfg ./yolo/config/
+mv data/vibe_data/coco.names ./yolo/data/
