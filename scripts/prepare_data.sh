e7c9211a7e28ca209e53e0d3aa06c731845e8397
Rodrigo Gebara Reis <reis.reisrodrigo@usp.br>
2022-08-05 19:44:27 -0400

    fixed prepare_data.sh

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -4,11 +4,11 @@ mkdir -p yolo/config
 mkdir -p yolo/data
 mkdir -p data
 cd data
-gdown "https://drive.google.com/uc?export=download&id=1JSLHXlOcgNCVu6mo2JFzzyj--7dioZnn"
+gdown "https://drive.google.com/uc?export=download&id=1Cfy1sQiKQC8bkjxbU37JU0oEaBp_Fpdb"
 unzip vibe_data.zip
 rm vibe_data.zip
 cd ..
-mv data/vibe_data/sample_video.mp4 .
+mv data/vibe_data/test.mp4 .
 mv data/vibe_data/yolov3.weights ./yolo/config/
 mv data/vibe_data/yolov3.cfg ./yolo/config/
 mv data/vibe_data/coco.names ./yolo/data/
